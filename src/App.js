1fecc8a4e637c24d0591c72ab4f21dc11da84eca
unknown <codeellix@gmail.com>
2024-01-16 11:37:54 +0200

    delete: footer and its imports

--- src/App.js
+++ src/App.js
@@ -3,15 +3,13 @@ import './App.css';
 import Header from './components/header';
 import Navbar from './components/navbar';
 import Profile from './components/profile'
-import Footer from './components/footer'
 function App() {
   return (
     <div className="app-wrapper"> 
      <Header />
      <Navbar />
      <Profile />
-     <Footer />
-    </div>
+    </div>s
   );
 }
 
