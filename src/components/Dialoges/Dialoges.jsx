29e38d69cc4a248a735f0ef20782a17a3b87ec08
unknown <codeellix@gmail.com>
2024-01-29 12:25:34 +0200

    feat: add less25

--- src/components/Dialoges/Dialoges.jsx
+++ src/components/Dialoges/Dialoges.jsx
@@ -25,6 +25,7 @@ let dialogesData = [
 {id: 5, name: "Ksenia"}
 ];
 
+
 let messageData = [
     {id: 1, message: "Hi"},
     {id: 2, message: "Hello"},
@@ -33,22 +34,18 @@ let messageData = [
     {id: 5, message: "Yo"}
     ];
 
+  let dialogesComponentData = dialogesData.map(el => <DialogItem id={el.id} name={el.name} />) 
+  let messagesComponentData = messageData.map(el => <Message id={el.id} message={el.message} />) 
+
 function Dialoges(props) {
     return(
-        <div className={style.ollDialogs}>
+        <div clascName={style.ollDialogs}>
+           
             <div className={style.dialogs}>
-                <DialogItem id={dialogesData[0].id} name={dialogesData[0].name} />
-                <DialogItem id={dialogesData[1].id} name={dialogesData[1].name} />
-                <DialogItem id={dialogesData[2].id} name={dialogesData[2].name} />
-                <DialogItem id={dialogesData[3].id} name={dialogesData[3].name} />
-                <DialogItem id={dialogesData[4].id} name={dialogesData[4].name} />
+                 {dialogesComponentData}
             </div>
             <div className={style.messages}>
-                <Message id={messageData[0].id} message={messageData[0].message} />
-                <Message id={messageData[1].id} message={messageData[1].message} />
-                <Message id={messageData[2].id} message={messageData[2].message} />
-                <Message id={messageData[3].id} message={messageData[3].message} />
-                <Message id={messageData[4].id} message={messageData[4].message} />
+            {messagesComponentData}
         </div>
     </div>
     )
